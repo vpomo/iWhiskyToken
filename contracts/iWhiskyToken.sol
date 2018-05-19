1e7e1529b5d5189459a6d31a8274141f1dc882f1
vpomo <pomogalov@gmail.com>
2018-05-19 12:20:47 +0300

    tested ok

--- contracts/iWhiskyToken.sol
+++ contracts/iWhiskyToken.sol
@@ -238,8 +238,8 @@ contract StandardToken is ERC20, ERC223Token {
 
 contract iWhiskyToken is StandardToken {
 
-    string public constant name = "Wine Token";
-    string public constant symbol = "iWINE";
+    string public constant name = "iWhisky Token";
+    string public constant symbol = "iWSK";
     uint8 public constant decimals = 18;
     uint256 public constant INITIAL_SUPPLY = 10**9 * (10**uint256(decimals));
     address public owner;
